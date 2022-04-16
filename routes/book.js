581760d9913a8648fddec5743fdfc86b33491fa6
Prabhat Kumar <pkkrsingh877@gmail.com>
2022-04-16 07:36:05 +0530

    basic CRUD app complete

--- routes/book.js
+++ routes/book.js
@@ -3,16 +3,36 @@ const router = express.Router();
 const path = require('path');
 const client = require('../models/database');
 
+router.patch('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { bookName, bookAuthor, bookImage } = req.body;
+    await client.query('UPDATE books SET bookname = $1, bookauthor = $2, bookimage = $3 WHERE id = $4 RETURNING *', [bookName, bookAuthor, bookImage, id]);
+    res.redirect(`/books/${id}`);
+})
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const result = await client.query('SELECT * FROM books WHERE id = $1', [id]);
+    const book = result.rows[0];
+    res.render('book/editBook', { book });
+});
+
 router.post('/', async (req, res) => {
     const { bookName, bookAuthor, bookImage } = req.body;
     const id = Math.floor(Math.random() * 10000000);
     await client.query('INSERT INTO books VALUES ($1, $2, $3, $4) RETURNING *', [id, bookName, bookAuthor, bookImage]);
-    console.log(bookName, bookAuthor, bookImage);
     res.redirect('/books');
 });
 
 router.get('/add', (req, res) => {
     res.render('book/addBook');
+});
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const result = await client.query('SELECT * FROM books WHERE id = $1', [id]);
+    const book = result.rows[0];
+    res.render('book/show', { book });
 });
 
 router.get('/', (req, res) => {
--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const ejsMate = require('ejs-mate');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
 
 //middlewares
 app.use(express.urlencoded({ extended: false }));
+app.use(morgan('tiny'));
+app.use(methodOverride('_method'))
 
 //ejs setup
 app.engine('ejs', ejsMate);
